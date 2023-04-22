a761fc426b2107c5d804fafbb445cc4fc1b02c5a
Paul Thomas <paul@asup.co.uk>
2023-04-22 19:38:49 +0100

    Update packages

--- jest.config.ts
+++ jest.config.ts
@@ -4,17 +4,33 @@ const config: Config = {
   // The root of your source code, typically /src
   // `<rootDir>` is a token Jest substitutes
   roots: ['<rootDir>', '<rootDir>/src'],
+  testEnvironment: 'jsdom',
 
   // Jest transformations -- this adds support for TypeScript
-  // using ts-jest
+  // using ts-jest, include ts-jest-mock-import-meta
   transform: {
-    '^.+\\.tsx?$': 'ts-jest',
+    '^.+\\.tsx?$': [
+      'ts-jest',
+      {
+        diagnostics: {
+          ignoreCodes: [1343],
+        },
+        astTransformers: {
+          before: [
+            {
+              path: 'ts-jest-mock-import-meta',
+              options: { metaObjectReplacement: { url: 'https://localhost' } },
+            },
+          ],
+        },
+      },
+    ],
   },
 
   // Runs special logic, such as cleaning up components
   // when using React Testing Library and adds special
   // extended assertions to Jest
-  setupFilesAfterEnv: ['@testing-library/jest-dom/extend-expect', './src/setupTests.ts'],
+  setupFilesAfterEnv: ['<rootDir>/src/setupTests.ts'],
   // Test spec file resolution pattern
   // Matches parent folder `__tests__` and filename
   // should contain `test` or `spec`.
@@ -23,7 +39,7 @@ const config: Config = {
   moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx'],
   // };
 
-  // Added by Paul
+  // Code coverage
   collectCoverage: true,
   coverageProvider: 'v8',
   collectCoverageFrom: [
@@ -34,10 +50,14 @@ const config: Config = {
     '!**/node_modules/**',
     '!**/vendor/**',
   ],
-  testEnvironment: 'jsdom',
+
+  // Map css type modules to blank module
   moduleNameMapper: {
-    '\\.(css|less)$': '<rootDir>/src/__mocks__/styleMock.ts',
+    '\\.(css|less|scss)$': '<rootDir>/src/__mocks__/styleMock.ts',
   },
+
+  // Plugin for watch patterns
+  watchPlugins: ['jest-watch-typeahead/filename', 'jest-watch-typeahead/testname'],
 };
 
 export default config;
